338242219e9ec1b32e2d0ad1785ef25435e433a9
LeatuyrBerty <phangialoc07@gmail.com>
2026-07-16 14:07:51 +0700

    config colorscheme for nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,31 @@
 return {
 	{
 		"craftzdog/solarized-osaka.nvim",
-		lazy = true,
+		lazy = false,
 		priority = 1000,
 		opts = function()
 			return {
 				transparent = true,
+				terminal_colors = true,
+				styles = {
+					comments = { italic = true },
+					keywords = { italic = true },
+					functions = {},
+					variables = {},
+					-- Background styles. Can be "dark", "transparent" or "normal"
+					sidebars = "transparent", -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+					floats = "transparent",
+				},
+				sidebars = { "qf", "vista_kind", "terminal", "packer" },
+				day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+				hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+				dim_inactive = false, -- dims inactive windows
+				lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
 			}
+		end,
+
+		init = function()
+			vim.cmd([[colorscheme solarized-osaka]])
 		end,
 	},
 }
