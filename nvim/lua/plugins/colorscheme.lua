f10f4f02a54dd072ff127228182c004f4e5a2ddd
LeatuyrBerty <phangialoc07@gmail.com>
2026-07-16 14:24:26 +0700

    turn on bold for lualine neovi

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -20,7 +20,12 @@ return {
 				day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
 				hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
 				dim_inactive = false, -- dims inactive windows
-				lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+				lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+				-- Change the "hint" color to the "orange" color, and make the "error" color bright red
+				on_colors = function(colors)
+					colors.hint = colors.orange
+					colors.error = "#ff0000"
+				end,
 			}
 		end,
 
